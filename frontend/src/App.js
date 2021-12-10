4ce470ef7899db2df62fba1c7c8a7bc20717616b
SamRocan <SamClendenan@iCloud.com>
2021-12-10 20:24:14 +0000

    Basic folder structure of frontend initialised

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
+import HeadingBannerComponent from "./components/HeadingBannerComponent";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+    constructor() {
+        super();
+        //Scrolls to top of page on refresh / load
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 id="title">Is this centered?</h1>
+                </header>
+                <HeadingBannerComponent/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ frontend/src/components/HeadingBannerComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HeadingBannerComponent extends Component{
+    render() {
+        return(
+        <div className="Test">
+            <h1>Here is the banner</h1>
+            <p>Some more banner content is here</p>
+        </div>
+        )
+    }
+}
+export default HeadingBannerComponent
