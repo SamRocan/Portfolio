6ac53cf5acf1ac2747b9686f938128e76ffe13d8
SamRocan <SamClendenan@iCloud.com>
2021-12-14 13:31:01 +0000

    Show names of projects on hover

--- frontend/src/components/ProjectObserverComponent.js
+++ frontend/src/components/ProjectObserverComponent.js
@@ -5,21 +5,13 @@ import './css/resume_style.css'
 
 const comp = (name) => {
 
-    let text = "ok"
-
-    function showText(e){
-        text = name.title.toString()
-        console.log(text)
-    }
-    function hideText(e){
-        text = ""
-    }
     return (
         <InView>
             {({ inView, ref, entry }) => (
                 <div ref={ref} className={`col-md-6 col-sm-12 project-${name.direction}-${inView}`}>
                     <a href={name.websiteLink}>
-                        <img src={name.imageLink} alt="Ok" width="100%" height="100%" onMouseEnter={showText} onMouseLeave={hideText}></img>
+                        <img src={name.imageLink} alt="Ok" width="100%" height="100%"></img>
+                        <h3 className="Test">{name.title}</h3>
                     </a>
                 </div>
             )}
