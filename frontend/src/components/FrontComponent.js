daae6524fac81d9d0b1b5e8a55b45e30303455fb
SamRocan <SamClendenan@iCloud.com>
2021-12-16 10:54:39 +0000

    Added Shapes to make the background less flat

--- frontend/src/components/FrontComponent.js
+++ frontend/src/components/FrontComponent.js
@@ -2,10 +2,17 @@ import React, {Component} from "react";
 import {Link} from 'react-scroll'
 import portrait from './images/IMG_0511.jpg';
 import {createRef} from "react/cjs/react.production.min";
+import backdrop from './images/backdrop.svg'
+import circle from './images/circle.svg'
 class FrontComponent extends Component {
     render() {
         return(
             <header className="App-header">
+                <div>
+                    <img id="circle-bg-1" src={circle}></img>
+                    <img id="circle-bg-2" src={circle}></img>
+                    <img id="circle-bg-3" src={circle}></img>
+                </div>
                 <p>Hello, I'm</p>
                 <div id="title-box">
                     <h1 id="title">Sam Clendenan</h1>
@@ -14,9 +21,8 @@ class FrontComponent extends Component {
                 <div className="container p-0 m-0">
                     <div className="row" id="info-summary">
                         <div className="col-md-6">
-                            <p>I'm a Full Stack Web Developer with a background in Finance and have recently completed a <a href="#">MSc Computer Science</a>
-                                <Link to="project-container" spy={true} smooth={true}><button className="btn btn-lrg" id="see-more">Find Out More</button></Link>
-                            </p>
+                            <p>I'm a Full Stack Web Developer with a background in Finance and have recently completed a <a href="#">MSc Computer Science</a></p>
+                            <Link to="project-container" spy={true} smooth={true}><button className="btn btn-lrg" id="see-more">Find Out More</button></Link>
                         </div>
                         <div className="col-md-6"  id="portrait-image">
                             <img src={portrait} width="100%"></img>
